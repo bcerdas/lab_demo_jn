1147b115e61db20908baaf450b7f11d4b6e28043
bcerdas <bryan.cerdas07@gmail.com>
2026-06-04 20:28:31 -0600

    first commit

---
+++ proyectofinal/src/main/java/com/mycompany/proyectofinal/Proyectofinal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectofinal;
+
+/**
+ *
+ * @author bryan
+ */
+public class Proyectofinal {
+
+    public static void main(String[] args) {
+        System.out.println("Proyecto final!");
+    }
+}
